52919cd4497e44d684e864040c26ab84c0f40443
Rafael Ribaldo <ribaldorafael@gmail.com>
2017-06-28 12:32:21 -0300

    Adding state_change_reason to exception message

--- qa_python_utils/aws/athena.py
+++ qa_python_utils/aws/athena.py
@@ -80,8 +80,9 @@ class AthenaClient(object):
             query_execution_status = self.__get_query_execution_status(query_execution_id)
 
         if query_execution_status in ('FAILED', 'CANCELLED'):
-            raise Exception('msg=query execution status {}, time_elapsed={}'.format(query_execution_status,
-                                                                                    time.time() - start_time))
+            raise Exception('status={}, time_elapsed={}, error_msg={}'.format(query_execution_status,
+                                                                                    time.time() - start_time,
+                                                                             query_execution['QueryExecution']['Status']['StateChangeReason']))
 
     def __get_query_execution_status(self, query_execution_id):
         query_execution = self.athena_client.get_query_execution(QueryExecutionId=query_execution_id)
