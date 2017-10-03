236af182d7887304b2bf3702fdbd8f0a96bc172f
Eizo Nishime <enishime@quintoandar.com.br>
2017-10-03 18:22:12 -0300

    Adding fallback for QA_PYTHON_UTILS_CREDENTIALS_JSON

--- qa_python_utils/google/server_credentials.py
+++ qa_python_utils/google/server_credentials.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 
 import json
 import os
+import logging
+
+log = logging.getLogger(__name__)
 
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -12,9 +15,16 @@ SCOPES = ['https://www.googleapis.com/auth/drive',
           'https://www.googleapis.com/auth/drive.metadata.readonly',
           'https://www.googleapis.com/auth/drive.photos.readonly',
           'https://www.googleapis.com/auth/drive.readonly']
-with open(os.getenv('QA_PYTHON_UTILS_CREDENTIALS_JSON', os.getenv('CREDENTIALS_JSON')),
-          'r') as content_file:  # noqa
-    CREDENTIALS_JSON = content_file.read()
+
+CREDENTIALS_JSON = None
+try:
+    with open(os.getenv('QA_PYTHON_UTILS_CREDENTIALS_JSON', os.getenv('CREDENTIALS_JSON')),
+              'r') as content_file:  # noqa
+        CREDENTIALS_JSON = content_file.read()
+except:
+    log.exception("Problem reading QA_PYTHON_UTILS_CREDENTIALS_JSON from file.")
+    if CREDENTIALS_JSON is None:
+        CREDENTIALS_JSON = os.getenv('QA_PYTHON_UTILS_CREDENTIALS_JSON', os.getenv('CREDENTIALS_JSON'))
 
 
 def get_credentials():
