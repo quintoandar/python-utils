cac74704990162ddb726467ee7b3113e194e8f63
Rafael Ribaldo <ribaldorafael@gmail.com>
2017-10-13 15:28:34 -0300

    Adding comments and 'exclude' param in decorator

--- qa_python_utils/default_logger.py
+++ qa_python_utils/default_logger.py
@@ -4,43 +4,62 @@ logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger('logger')
 
 
-def logger(func):
-    def wrapper(*args, **kwargs):
-        logging_string = 'm={}'
+def logger(exclude=None):
+    """
+    Usage:
+        @logger
+        def some_function([cls, self, None], x, y):
+            pass
+        
+        @logger(exclude='x')
+        def some_function(x, y):
+            pass
+            
+        @logger(exclude=['x', 'y']):
+            pass
+    :param exclude: param names to be excluded in the logging string
+    :return: complete logging string
+    """
+    
+    def _logger_func(func):
+        def _wrapper(*args, **kwargs):
+            logging_string = 'm={}'
 
-        if args and len(args) > 1:
-            complete_args = ''
-            has_multiple_params = len(func.__code__.co_varnames) < len(args)
-            for index, arg in enumerate(args):
-                if index >= len(func.__code__.co_varnames):
-                    complete_args += ('{})' if index == len(args) - 1 else '{}, ').format(arg)
-                    continue
+            if args and len(args) > 1:
+                complete_args = ''
+                has_multiple_params = len(func.__code__.co_varnames) < len(args)
+                for index, arg in enumerate(args):
+                    if index >= len(func.__code__.co_varnames):
+                        complete_args += ('{})' if index == len(args) - 1 else '{}, ').format(arg)
+                        continue
 
-                arg_name = func.__code__.co_varnames[index]
-                if 'self' in arg_name:
-                    continue
+                    arg_name = func.__code__.co_varnames[index]
+                    if 'self' in arg_name or arg_name in exclude:
+                        continue
 
-                complete_args += ('*{}=({}, ' if len(args) > func.__code__.co_argcount
-                                                 and index >= func.__code__.co_argcount else '{}={}, ').format(arg_name,
-                                                                                                               arg)
+                    complete_args += ('*{}=({}, ' if len(args) > func.__code__.co_argcount
+                                                     and index >= func.__code__.co_argcount else '{}={}, ').format(arg_name,
+                                                                                                                   arg)
 
-            if len(complete_args) > 0 and not has_multiple_params:
-                complete_args = complete_args[:-2]
-                complete_args += ')' if len(args) > func.__code__.co_argcount else ''
+                if len(complete_args) > 0 and not has_multiple_params:
+                    complete_args = complete_args[:-2]
+                    complete_args += ')' if len(args) > func.__code__.co_argcount else ''
 
-            logging_string += ', {}'.format(complete_args)
+                if len(complete_args) > 0:
+                    logging_string += ', {}'.format(complete_args)
 
-        if kwargs and len(kwargs) > 0:
-            logging_string += ', kwargs={}'
+            if kwargs and len(kwargs) > 0:
+                logging_string += ', kwargs={}'
 
-        if (not args or len(args) <= 1) and (not kwargs and len(kwargs) <= 0):
-            logging_string += ', msg=init'
+            if (not args or len(args) <= 1) and (not kwargs and len(kwargs) <= 0):
+                logging_string += ', msg=init'
 
-        _logger.info(logging_string.format(func.__name__, kwargs))
-        try:
-            return func(*args, **kwargs)
-        except:
-            _logger.exception('m={}, msg=exception'.format(func.__name__))
-            raise
+            _logger.info(logging_string.format(func.__name__, kwargs))
+            try:
+                return func(*args, **kwargs)
+            except:
+                _logger.exception('m={}, msg=exception'.format(func.__name__))
+                raise
 
-    return wrapper
+        return _wrapper
+    return _logger_func
