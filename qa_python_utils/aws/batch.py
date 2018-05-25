d709272c96ec72bd2f358583b9923eac63a6c28d
Rafael Ribaldo <ribaldorafael@gmail.com>
2018-05-25 11:23:23 -0300

    Refactoring aws.batch

--- qa_python_utils/aws/batch.py
+++ qa_python_utils/aws/batch.py
@@ -1,7 +1,6 @@
 import sys
 
 import boto3
-
 from qa_python_utils.default_logger import logger, _logger
 
 reload(sys)
@@ -18,18 +17,53 @@ class BatchClient(object):
         self.batch_client = boto3.client('batch')
 
     @logger
-    def __has_job_exceeded_max_running(self, job_name, job_queue, max_running_jobs):
+    def get_running_jobs_list(self, job_name, job_queue):
         all_running_jobs = self.batch_client.list_jobs(
             jobQueue=job_queue,
             jobStatus='RUNNING',
             maxResults=1000
         )
 
-        running_jobs = [job_summary['jobName'] for job_summary in all_running_jobs['jobSummaryList'] if
-                        job_summary['jobName'] == job_name]
+        return [job_summary['jobName'] for job_summary in all_running_jobs['jobSummaryList'] if
+                job_summary['jobName'] == job_name]
 
-        _logger.info('m=__check_running_jobs, running_jobs={}'.format(len(running_jobs)))
-        return len(running_jobs) >= max_running_jobs
+    @logger
+    def __compare_running_instances(self, job_name, job_queue, comparison_value):
+        running_jobs = self.get_running_jobs_list(
+            job_name=job_name,
+            job_queue=job_queue
+        )
+
+        _logger.info(
+            'm=__compare_running_instances, comparison_result={}'.format(len(running_jobs) >= comparison_value))
+        return len(running_jobs) >= comparison_value
+
+    @logger
+    def is_job_running(self, job_name, job_queue):
+        comparison_result = self.__compare_running_instances(
+            job_name=job_name,
+            job_queue=job_queue,
+            comparison_value=1
+        )
+
+        _logger.info('m=is_job_running, is_running={}'.format(comparison_result))
+        return comparison_result
+
+    @logger
+    def has_job_exceeded_max_running(self, job_name, job_queue, max_running_jobs):
+        comparison_result = self.__compare_running_instances(
+            job_name=job_name,
+            job_queue=job_queue,
+            comparison_value=max_running_jobs
+        )
+
+        running_jobs = self.get_running_jobs_list(
+            job_name=job_name,
+            job_queue=job_queue
+        )
+
+        _logger.info('m=has_job_exceeded_max_running, running_jobs={}'.format(len(running_jobs)))
+        return comparison_result
 
     @logger
     def start_batch_job(self, job_name, job_queue, job_definition, command=None, vcpus=4, memory=4096,
@@ -38,7 +72,7 @@ class BatchClient(object):
             _logger.error('m=start_batch_job, command={}, msg=wrong params'.format(command))
             return
 
-        exceeded_max_running = self.__has_job_exceeded_max_running(
+        exceeded_max_running = self.has_job_exceeded_max_running(
             job_name=job_name,
             job_queue=job_queue,
             max_running_jobs=max_running_jobs
