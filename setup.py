38649f45a1358e33cbaa30c0ce3df770ecc9fd4f
Ribaldo <ribaldorafael@gmail.com>
2018-06-28 16:59:59 -0300

    Refactoring setup and upgrading version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='qa-python-utils',
-    version='0.0.3',
+    version='0.1.0',
     packages=['qa_python_utils',
               'qa_python_utils.google',
               'qa_python_utils.aws'],
     url='https://github.com/quintoandar/python-utils',
     license='',
-    author='Quinto Andar',
+    author='QuintoAndar',
     author_email='enishime@quintoandar.com.br',
     description='Package for python utils',
     install_requires=[open('requirements.txt').read().strip().split('\n')]
