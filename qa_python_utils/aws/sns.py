0cb420a1401813188c42d37d2e3a2449b3b00089
Thiago Pinto <thiago.pinto@quintoandar.com.br>
2018-09-14 15:07:12 -0300

    Remove dummy aws keys

--- qa_python_utils/aws/sns.py
+++ qa_python_utils/aws/sns.py
@@ -10,10 +10,7 @@ logger = QuintoAndarLogger('aws.sns')
 
 
 default_kwargs = {
-    'region_name': os.getenv('AWS_REGION', 'us-east-1'),
-    'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID', 'dummy'),
-    'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY',
-                                       'dummy')}
+    'region_name': os.getenv('AWS_REGION', 'us-east-1')}
 
 
 class JsonDecoder(json.JSONEncoder):
