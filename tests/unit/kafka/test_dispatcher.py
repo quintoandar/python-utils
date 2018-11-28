159777e347add240f4eb6599c2d9564a4c7963c4
fabiothiroki <fabiothiroki@gmail.com>
2018-11-28 17:48:07 -0200

    Test dispatch message

--- tests/unit/kafka/test_dispatcher.py
+++ tests/unit/kafka/test_dispatcher.py
@@ -6,3 +6,20 @@ def test_instance():
     mock_producer = Mock()
     dispatcher = KafkaDispatcher(mock_producer)
     assert dispatcher.kafka_producer == mock_producer
+
+
+def test_dispatch_message():
+    mock_producer = Mock()
+    dispatcher = KafkaDispatcher(mock_producer)
+
+    dispatcher.dispatch_message('topic', 'event', 'payload', 'source', 10)
+
+    mock_producer.send.assert_called_once_with(
+        'topic', {
+            'topic': 'topic',
+            'eventName': 'event',
+            'payload': 'payload',
+            'source': 'source'
+        })
+
+    mock_producer.flush.assert_called_once_with(10)
