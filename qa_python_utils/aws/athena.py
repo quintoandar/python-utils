e733850016f60c860548ab020392f1dd0378d746
Juliana Freire <juliana.freire@quintoandar.com.br>
2020-02-18 19:15:20 -0300

    adding aws key and secret to boto resource s3

--- qa_python_utils/aws/athena.py
+++ qa_python_utils/aws/athena.py
@@ -22,7 +22,7 @@ logger = QuintoAndarLogger('aws.athena')
 
 
 class AthenaClient(object):
-    @logger
+    @logger(exclude=["aws_access_key_id", "aws_secret_access_key"])
     def __init__(self, s3_bucket=None, bucket_folder_path='query_results', aws_access_key_id=None,
                  aws_secret_access_key=None):
         self.s3_bucket = s3_bucket
@@ -30,10 +30,12 @@ class AthenaClient(object):
         if aws_access_key_id is not None and aws_secret_access_key is not None:
             self.athena_client = boto3.client('athena', aws_access_key_id=aws_access_key_id,
                                               aws_secret_access_key=aws_secret_access_key)
+            self.s3_resource = boto3.resource('s3', aws_access_key_id=aws_secret_access_key,
+                                              aws_secret_access_key=aws_secret_access_key)
         else:
             self.athena_client = boto3.client('athena')
+            self.s3_resource = boto3.resource('s3')
 
-        self.s3_resource = boto3.resource('s3')
         self.bucket_folder_path = bucket_folder_path
 
     @logger
